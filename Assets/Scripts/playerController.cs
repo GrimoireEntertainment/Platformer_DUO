3bb4e9fe6cbf347a547cfc8834dafb018ad6f880
Raistlin <mr.otashbek@inbox.ru>
2020-02-29 23:28:20 +0500

    Работа Шукура Модернизировал Оташ

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+
+    [SerializeField] float maxSpeed;
+    [SerializeField] float jumpHeight;
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] Transform groundChecker;
+    private float groundCheckRadius = 0.3f;
+    Rigidbody2D myRB;
+    bool facingRight;
+    bool isGrounded;
+    
+    
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+        facingRight = true;
+    }
+
+    void Update()
+    {
+        Jumping();
+    }
+
+    void FixedUpdate()
+    {
+        Moving();
+    }
+
+
+
+    private void Moving()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundChecker.position, groundCheckRadius, groundLayer);
+
+        float move = Input.GetAxis("Horizontal");
+
+        myRB.velocity = new Vector2(move * maxSpeed, myRB.velocity.y);
+
+        if (move > 0 && !facingRight) flip();
+        if (move < 0 && facingRight)  flip();
+    }
+
+    private void Jumping()
+    {
+        if (isGrounded && Input.GetAxis("Jump") > 0)
+        {
+            isGrounded = false;
+            myRB.AddForce(new Vector2(0, jumpHeight));
+        }
+    }
+
+    private void flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scale = transform.localScale;
+        Scale.x *= -1;
+        transform.localScale = Scale;
+    }
+}
