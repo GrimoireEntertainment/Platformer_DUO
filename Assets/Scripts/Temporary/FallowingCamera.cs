289604e230a74b537469ab3f1fb9da1b1c664525
Raistlin <mr.otashbek@inbox.ru>
2020-03-01 13:57:09 +0500

    Добавил движение камеры, поменял спрайты

---
+++ Assets/Scripts/Temporary/FallowingCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallowingCamera : MonoBehaviour
+{
+    [SerializeField] Transform Character;
+    [SerializeField] Vector3 offset;
+    [SerializeField] [Range(0,1)] float smooth = 0.2f;
+
+    void FixedUpdate()
+    {
+        Fallowing();
+    }
+
+    private void Fallowing()
+    {
+        Vector3 characterPosition = Character.position + offset;
+        transform.position = Vector3.Lerp(transform.position, characterPosition, smooth);
+    }
+}
