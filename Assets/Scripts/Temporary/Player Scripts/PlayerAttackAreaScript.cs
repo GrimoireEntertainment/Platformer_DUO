bdc3f325d8e174fa88da83f3d1465857d310ba3c
Raistlin <mr.otashbek@inbox.ru>
2020-03-20 08:53:26 +0500

    Поменял кнопку атаки с мышки на кнопку "К" изза конфликта

--- Assets/Scripts/Temporary/Player Scripts/PlayerAttackAreaScript.cs
+++ Assets/Scripts/Temporary/Player Scripts/PlayerAttackAreaScript.cs
@@ -10,8 +10,9 @@ public class PlayerAttackAreaScript : MonoBehaviour
     float nextDamage = 0.0f;
 
     private void OnTriggerStay2D(Collider2D other) {
-        
-        if((Input.GetKey(KeyCode.Mouse0) || swordAttackButton.isPressed) && other.tag == "Enemy" && Time.time > nextDamage) {
+        //Конфликт - Input.GetKey(KeyCode.Mouse0) и swordAttackButton.isPressed
+        // насколько я понял на телефоне простое нажатие по экрану воспринимается как Input.GetKey(KeyCode.Mouse0)
+        if((Input.GetKey(KeyCode.K) || swordAttackButton.isPressed) && other.tag == "Enemy" && Time.time > nextDamage) {
             Health enemyHealth = other.gameObject.GetComponent<Health>();
             enemyHealth.AddDamage(playerDamage);
             nextDamage = Time.time + damageRate;
