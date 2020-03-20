71c29486617dbe6dc8edb6168d87c87b11be5c29
Raistlin <mr.otashbek@inbox.ru>
2020-03-20 22:10:36 +0500

    Checking build version

---
+++ Assets/Scripts/Temporary/Player Scripts/DisplayHP.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayHP : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        float hp = GameObject.FindWithTag("Player").GetComponentInChildren<Health>().currentHealth;
+        transform.GetComponent<Text>().text = hp.ToString();
+    }
+}
---
+++ Assets/Scripts/Temporary/Player Scripts/Health.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] float maxHealth;
+    public float currentHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void AddDamage(float damage) {
+        currentHealth -= damage;
+        if(currentHealth <= 0) Destroy(gameObject);
+    }
+}
