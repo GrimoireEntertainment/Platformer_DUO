bdb8980425da6226ec7d81566c90dbe5c729dd0a
hiki01 <52518844+hiki01@users.noreply.github.com>
2020-03-21 21:39:32 +0500

    Тест на конфликт

--- Assets/Scripts/Temporary/Player Scripts/Health.cs
+++ Assets/Scripts/Temporary/Player Scripts/Health.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    [SerializeField] float maxHealth;
     public float currentHealth;
     // Start is called before the first frame update
     void Start()
     {
-        currentHealth = maxHealth;
+        
     }
 
     public void AddDamage(float damage) {
         currentHealth -= damage;
-        if(currentHealth <= 0) Destroy(gameObject);
+        // if(currentHealth <= 0) Destroy(gameObject);
     }
 }
