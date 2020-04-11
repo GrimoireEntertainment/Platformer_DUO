b80d45a9bfb7b2541c284408849c0cd7f2a1ee54
Raistlin <mr.otashbek@inbox.ru>
2020-04-11 14:25:13 +0500

    Ft interface levels (#23)
    * Меню для выбора уровня завершен!
    
    * Sort of menu
    
    Co-authored-by: hiki01 <52518844+hiki01@users.noreply.github.com>

---
+++ Assets/Scripts/Temporary/Core/SceneLoading.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoading : MonoBehaviour
+{
+    [SerializeField] string loadScene = "";
+    public void OnMouseDown()
+    {
+        SceneManager.LoadScene(loadScene);
+    }
+}
