300b3c049a4f9932f2117e381280b64d35081a04
hiki01 <52518844+hiki01@users.noreply.github.com>
2020-05-27 16:27:01 +0500

    Баг с толканием камня игроком Y исправлен. (#30)

--- Assets/Scripts/Temporary/Interactive Objects/pushObject.cs
+++ Assets/Scripts/Temporary/Interactive Objects/pushObject.cs
@@ -6,6 +6,9 @@ public class pushObject : MonoBehaviour
 {
     [SerializeField] float massForPlayerX;
     [SerializeField] float massForPlayerY;
+
+    bool isPlayerHere;
+    char xOrY;
     Rigidbody2D myRB;
 
     // Start is called before the first frame update
@@ -17,20 +20,34 @@ public class pushObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(isPlayerHere) 
+        {
+            xOrY = GameObject.FindWithTag("Player").GetComponentInChildren<Stats>().xOrY;
+            if(xOrY == 'x')
+            {
+                myRB.mass = massForPlayerX;
+            }
+
+            if (xOrY == 'y')
+            {
+                myRB.mass = massForPlayerY;
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
         {
-            char xOrY = other.GetComponent<PlayerController>().xOrY;
-            if(xOrY == 'x')
-                myRB.mass = massForPlayerX;
-
-            if (xOrY == 'y')
-                myRB.mass = massForPlayerY;
+            isPlayerHere = true;
         }
-        
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            isPlayerHere = false;
+        }
     }
 }
